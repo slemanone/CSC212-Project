2067cf86c496e64b729a945d58478e8c0223b96e
SalehAD78 <145998115+SalehAD78@users.noreply.github.com>
2023-10-11 18:57:42 +0300

    Node class

--- CSC212/src/Node.java
+++ CSC212/src/Node.java
@@ -1,4 +1,36 @@
 
-public class Node {
+public class Node<T> {
+public Node<T> next;
+public T data;
 
+//  Default Constructor 
+public Node() {
+	data = null;
+	next=null;	
+}
+
+//parameterized Constructor
+public Node (T val) {
+	data=val;
+	next=null;
+	
+}
+
+// Setters and Getters 
+
+public Node<T> getNext() {
+	return next;
+}
+
+public void setNext(Node<T> next) {
+	this.next = next;
+}
+
+public T getData() {
+	return data;
+}
+
+public void setData(T data) {
+	this.data = data;
+}
 }
